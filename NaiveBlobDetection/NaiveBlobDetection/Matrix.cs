6b0aecedff5340930bc5e2a47def114e90d337db
Andrey Dodonov <aadodonov@gmail.com>
2019-06-01 13:43:57 +0200

    Add C# project for naive blob detection, add random 0/1 matrix generation

---
+++ NaiveBlobDetection/NaiveBlobDetection/Matrix.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IslandAlgorithm
+{
+    class Matrix<T>
+    {
+        private List<List<T>> _matrix;
+        public Matrix(int size = 0)
+        {
+            _matrix = new List<List<T>>(size);
+        }
+
+        public void AddRow(List<T> row)
+        {
+            _matrix.Add(row);
+        }
+    }
+}
---
+++ NaiveBlobDetection/NaiveBlobDetection/MatrixFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IslandAlgorithm
+{
+    class MatrixFactory
+    {
+        private Random rand;
+        public MatrixFactory()
+        {
+            rand = new Random();
+        }
+
+        public Matrix<int> GetRandomMatrix(int size, int min, int max)
+        {
+            Matrix<int> matrix = new Matrix<int>(size);
+
+            for (int i=0; i<size; i++)
+            {
+                List<int> row = new List<int>();
+                for (int j=0; j<size; j++)
+                {
+                    row.Add(rand.Next(min, max));
+                }
+
+                matrix.AddRow(row);
+            }
+            return matrix;
+        }
+    }
+}
