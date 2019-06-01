5c169e3a956dc5eb20799a2cc2af902d3753d4c7
Andrey Dodonov <aadodonov@gmail.com>
2019-06-01 14:16:04 +0200

    Display generated random black-and-white matrix

--- NaiveBlobDetection/NaiveBlobDetection/Display.cs
+++ NaiveBlobDetection/NaiveBlobDetection/Display.cs
@@ -12,9 +12,32 @@ namespace IslandAlgorithm
 {
     public partial class Display : Form
     {
+        private MatrixFactory _matrixFactory;
         public Display()
         {
             InitializeComponent();
+            _matrixFactory = new MatrixFactory();
+            CreateBitmapAtRuntime();
+        }
+
+        public void CreateBitmapAtRuntime()
+        {
+            Bitmap randomBlackWhiteImage = new Bitmap(pictureBoxSource.Width, pictureBoxSource.Height);
+            Graphics gfx = Graphics.FromImage(randomBlackWhiteImage);
+            Matrix<int> randomBlackWhiteMatrix = _matrixFactory.GetRandomMatrix(pictureBoxSource.Width, pictureBoxSource.Height, 0, 2); // [0, 2)
+
+            for (int y = 0; y < randomBlackWhiteMatrix.Height; y++)
+            {
+                for (int x = 0; x < randomBlackWhiteMatrix.Width; x++)
+                {
+                    if (randomBlackWhiteMatrix[x,y] == 0)
+                        gfx.FillRectangle(Brushes.Black, new Rectangle(x, y, 1, 1));
+                    else
+                        gfx.FillRectangle(Brushes.White, new Rectangle(x, y,1, 1));
+                }
+            }
+            pictureBoxSource.Image = randomBlackWhiteImage;
+
         }
     }
 }
